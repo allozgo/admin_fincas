b55923fce1a991c7ea0b657702776013db631897
allozgo <celamino@hotmail.com>
2024-01-15 21:31:29 +0100

    update

--- app.py
+++ app.py
@@ -42,9 +42,10 @@ def prueba():
 
         resumen = query({"inputs": text})
         contenido_resumen = resumen[0][next(iter(resumen[0]))]
-        resumen_collection.insert_one({'resumen': contenido_resumen})
+        texto = contenido_resumen
+        resumen_collection.insert_one({'resumen': texto})
         
-        return jsonify({'resumen': contenido_resumen}) 
+        return jsonify({'resumen': texto}) 
 
     except Exception as e:
         # Log the exception for debugging
