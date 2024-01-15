afff4efe4a6945c5e6b2a974eab286f1464db25a
allozgo <celamino@hotmail.com>
2024-01-15 15:32:56 +0100

    update

--- app.py
+++ app.py
@@ -1,16 +1,42 @@
+from flask import Flask, jsonify, render_template, request
+from gtts import gTTS
+import requests
+import os 
+from pymongo import MongoClient 
+from pdfminer.high_level import extract_text 
+
+app = Flask(__name__)
+
+#conexiones con BBDD
+mongo_url ='mongodb+srv://falberola:5zZi7xSEYPPIdGgc@cluster0.hd9lmf3.mongodb.net/datadmin_fincas'
+client = MongoClient(mongo_url)
+db = client.get_database('datadmin_fincas')
+resumen_collection = db['resumen'] 
+audios_collection = db['audios'] 
+
+@app.route('/', methods=['GET'])
+def plantilla():
+    return render_template('endpoints.html')
+
 @app.route('/subir_pdf', methods=['POST'])
 def subir_pdf():
     if 'file' not in request.files:
-        return "No se proporcionó ningún archivo", 400
+        return "No se proporcionó ningún archivo"
 
     file = request.files['file']
 
     if not file:
-        return "El archivo está vacío", 400
+        return "El archivo está vacío"
 
     # Ubicación temporal
     file_path = os.path.join("temp", "uploaded_file.pdf")
     file.save(file_path)
+
+    fs_pdf = GridFS(db, collection='pdfs')
+
+    with open(file_path, 'rb') as pdf_file:
+    # Save the binary content to GridFS
+    file_id = fs_pdf.put(pdf_file, filename='acta.pdf', metadata={'folder': 'pdfs'})
 
     return "Archivo recibido correctamente", 200
 
@@ -22,6 +48,8 @@ def resumen():
     #file_content = request.args.get('file_content')
 
     file_path = os.path.join("temp", "uploaded_file.pdf")
+
+    file = request.files['file']
 
     if not os.path.exists(file_path):
         return "No se encontró el archivo", 400
@@ -46,3 +74,6 @@ def resumen():
     audios_collection.insert_one({'audio': audio})
     
     return jsonify({'resumen': contenido_resumen, 'audio': audio})
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8000)
