ef3579229bc55484c4aa6de4af52e12254160ac9
allozgo <celamino@hotmail.com>
2024-01-15 18:36:08 +0100

    update

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ def prueba():
         f.save(f.filename)   
         return render_template("Acknowledgement.html", name = f.filename)  
 
-@app.route('/subir_pdf', methods=['POST'])
-def subir_pdf():
+@app.route('/resumen', methods=['POST'])
+def resumen():
     text = extract_text('./Acta comunidad.pdf')
     local_pdf_file = './Acta comunidad.pdf'
     #raw = parser.from_file('./texto1.pdf')
@@ -72,40 +72,51 @@ def subir_pdf():
 
     return jsonify({'resumen': contenido_resumen})
 
-@app.route('/resumen', methods=['GET','POST'])
-def resumen():
-    #if 'file' not in request.files:
-    #    return "No se proporcionó ningún archivo"
-    #file = request.files['file']
-    #file_content = request.args.get('file_content')
+@app.route('/audio', methods=['GET','POST'])
+def audio():
+    text = extract_text('./Acta comunidad.pdf')
+    local_pdf_file = './Acta comunidad.pdf'
+    #raw = parser.from_file('./texto1.pdf')
 
-    file_path = os.path.join("temp", "uploaded_file.pdf")
-
-    file = request.files['file']
-
-    if not os.path.exists(file_path):
-        return "No se encontró el archivo", 400
-    if not file:
-        return "No se proporcionó ningún archivo en el parámetro 'file'", 400
-
-    text = extract_text(file_path)
-    
     API_TOKEN = "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"
+
     API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
     headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
     def query(payload):
         response = requests.post(API_URL, headers=headers, json=payload)
         return response.json()
+        
+    resumen = query({"inputs":text})
 
-    resumen = query({"inputs": text})
     contenido_resumen = resumen[0][next(iter(resumen[0]))]
-    resumen_collection.insert_one({'resumen': contenido_resumen})
-    tts = gTTS(text=contenido_resumen, lang='es')
-    audio = tts.save("audio.mp3")
-    audios_collection.insert_one({'audio': audio})
-    
-    return jsonify({'resumen': contenido_resumen, 'audio': audio})
+
+    texto = contenido_resumen
+
+    tts = gTTS(text=texto, lang='es')
+
+    tts.save("audio.mp3")
+
+
+    resumen_collection.insert_one({'resumen': texto})
+
+    local_audio_file = './audio.mp3'
+
+    # Connect to the GridFS collection
+    fs_pdf = GridFS(db, collection='pdfs')
+    fs_audio = GridFS(db, collection='audios')
+
+    # Open the local audio file in binary mode ('rb')
+    with open(local_audio_file, 'rb') as audio_file:
+        # Save the binary content to GridFS
+        audio_file_id = fs_audio.put(audio_file, filename='audio.mp3', metadata={'folder': 'audios'})
+
+    with open(local_pdf_file, 'rb') as pdf_file:
+        # Save the binary content to GridFS
+        pdf_file_id = fs_pdf.put(pdf_file, filename='acta.pdf', metadata={'folder': 'pdfs'})
+    #audios_collection.insert_one({'audio': audio_binario})
+
+    return send_file(local_audio_file, mimetype='audio/mp3', as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
