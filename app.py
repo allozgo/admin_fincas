72c5f3fc67fcec7683414bc397fabce0b58acc87
allozgo <celamino@hotmail.com>
2024-01-15 10:43:46 +0100

    update

--- app.py
+++ app.py
@@ -26,14 +26,14 @@ def plantilla():
 
 @app.route('/subir_pdf', methods=['POST'])
 def subir_pdf():
-    if 'archivo' not in request.files:
+    if 'file' not in request.files:
         return "No se proporcionó ningún archivo"
     file = request.files['file']
     return file
 
 @app.route('/resumen', methods=['GET','POST'])
 def resumen():
-    if 'archivo' not in request.files:
+    if 'file' not in request.files:
         return "No se proporcionó ningún archivo"
     file = request.files['file']
     text = extract_text(file)
