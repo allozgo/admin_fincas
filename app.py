dd6f2e7343f5ce51e88589ffe7934c6fe4d534b9
allozgo <celamino@hotmail.com>
2024-01-15 23:56:23 +0100

    update

--- app.py
+++ app.py
@@ -51,52 +51,60 @@ def prueba():
 
 @app.route('/resumen', methods=['GET'])
 def resumen():
-    file = temp_file_storage.get('uploaded_file')
+    try:
+        file = temp_file_storage.get('uploaded_file')
 
-    reader = PdfReader(file)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
+        reader = PdfReader(file)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text() + "\n"
 
-    API_TOKEN = "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"
+        API_TOKEN = "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"
 
-    API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
-    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+        API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+        headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
-    def query(payload):
-        response = requests.post(API_URL, headers=headers, json=payload)
-        return response.json()
-        
-    resumen = query({"inputs":text})
+        def query(payload):
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.json()
+            
+        resumen = query({"inputs":text})
 
-    contenido_resumen = resumen[0][next(iter(resumen[0]))]
+        contenido_resumen = resumen[0][next(iter(resumen[0]))]
 
-    texto = contenido_resumen
+        texto = contenido_resumen
 
-    tts = gTTS(text=texto, lang='es')
+        tts = gTTS(text=texto, lang='es')
 
-    tts.save("audio.mp3")
+        tts.save("audio.mp3")
 
 
-    resumen_collection.insert_one({'resumen': texto})
+        resumen_collection.insert_one({'resumen': texto})
 
-    local_audio_file = './audio.mp3'
+        local_audio_file = './audio.mp3'
 
-    # Connect to the GridFS collection
-    fs_pdf = GridFS(db, collection='pdfs')
-    fs_audio = GridFS(db, collection='audios')
+        # Connect to the GridFS collection
+        fs_pdf = GridFS(db, collection='pdfs')
+        fs_audio = GridFS(db, collection='audios')
 
-    # Open the local audio file in binary mode ('rb')
-    with open(local_audio_file, 'rb') as audio_file:
-        # Save the binary content to GridFS
-        audio_file_id = fs_audio.put(audio_file, filename='audio.mp3', metadata={'folder': 'audios'})
+        # Open the local audio file in binary mode ('rb')
+        with open(local_audio_file, 'rb') as audio_file:
+            # Save the binary content to GridFS
+            audio_file_id = fs_audio.put(audio_file, filename='audio.mp3', metadata={'folder': 'audios'})
 
-    with open(local_pdf_file, 'rb') as pdf_file:
-        # Save the binary content to GridFS
-        pdf_file_id = fs_pdf.put(pdf_file, filename='acta.pdf', metadata={'folder': 'pdfs'})
-    #audios_collection.insert_one({'audio': audio_binario})
+        with open(local_pdf_file, 'rb') as pdf_file:
+            # Save the binary content to GridFS
+            pdf_file_id = fs_pdf.put(pdf_file, filename='acta.pdf', metadata={'folder': 'pdfs'})
+        #audios_collection.insert_one({'audio': audio_binario})
 
-    return jsonify({'resumen': texto})
+        return jsonify({'resumen': texto})
+    
+    except Exception as e:
+        # Log the exception for debugging
+        print(f"Error: {str(e)}")
+        error_response = jsonify(error=str(e))
+        error_response.status_code = 500
+        return error_response
 
 @app.route('/audio', methods=['GET','POST'])
 def audio():
